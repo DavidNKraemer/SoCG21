1cc8cc46d19bcd19ab5896ba078b7b98ec07d75a
David Kraemer <david.kraemer@stonybrook.edu>
2021-01-06 18:32:36 -0500

    Test script modeling the behavior

--- src/test_script.py
+++ src/test_script.py
@@ -1,26 +1,25 @@
 import numpy as np
-from agent import *
+from board import *
+
+np.random.seed(0)
 
 # instantiate Agents
-a = Agent(np.array([0, 0]), np.array([5, 5]))
-b = Agent(np.array([5, 0]), np.array([0, 5]))
-c = Agent(np.array([0, 5]), np.array([5, 0]))
-d = Agent(np.array([5, 5]), np.array([0, 0]))
+sources = np.random.randint(-10,10, size=(4,2))
+targets = np.random.randint(-10,10, size=(4,2))
 
 # pre-process local neighborhoods using magic in DistributedState's __init__
-board = DistributedBoard(set([a, b, c, d]), obstacles)
-policy = Policy(board)
-
-# move an agent... God told us the best move to make (how nice of Him!)
-board.direct(b, "N")  # affected Agents' local neighborhoods updated magically
+board = DistributedBoard(sources, targets, [])
+policy = DumbPolicy(board)
 
 ##################################
+t = 0
 while not board.isdone():
     # priority queue to find which move to make (God hides his face)
-    board.populate_queue()
-    for agent in board.queue:
-        # get instruction
-        direction = policy(agent)
-        board.direct(agent, direction)
+    print(f't = {t}')
+    agent = board.pop()
+    direction = policy(agent)
+    agent.move(direction)
+    board.insert(agent)
+    t += 1
 
 # key objects: Agent, DistributedBoard, Policy, (priority_queue?)
