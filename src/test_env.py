9fc7b062c9190bd4070dff1648bce6ccfad71be7
David Kraemer <david.kraemer@stonybrook.edu>
2021-01-26 14:16:26 -0500

    Minor documentation

--- src/test_env.py
+++ src/test_env.py
@@ -3,23 +3,23 @@ import numpy as np
 
 from envs import BoardEnv
 
-sources = np.array([
-    [0,0]
-])
+# list of source positions
+sources = np.array([[0,0]])
 
-targets = np.array([
-    [5,0]
-])
+# list of target positions
+targets = np.array([[5,0]])
 
+# list of obstacles
 obstacles = []
 
+# BoardEnv initialization
 env = BoardEnv(sources, targets, obstacles)
 env.reset()
 
 done = False
 
 while not done:
-    action = 0
+    action = 0  # <--- this is where the magic should happen
     state, reward, done, _ = env.step(action)
     print(state)
 
