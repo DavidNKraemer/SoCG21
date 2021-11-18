a52c24b2092f647b7641ec74360ed81db2e66553
LoganDGraham <logan.graham@stonybrook.edu>
2021-11-18 12:10:01 -0500

    petting zoo demo suggests that env is working

--- scripts/pz_demo.py
+++ scripts/pz_demo.py
@@ -1,49 +1,38 @@
-import os
 import sys
 
-sys.path.append(
-    '..'
-)
+sys.path.append("/home/logan/Documents/Code/SoCG21/")
 
 import numpy as np
-from src.envs import PZBoardEnv, bot_reward
-from IPython import embed
-
-
-li = 0
+from src.envs import PZBoardEnv
 
 
 def left_policy():
-    global li
-    out = 2 * (li % 2)
-    li += 1
-    return out
-
-
-ri = 0
+    if np.random.uniform() > 0.5:
+        return 0
+    else:
+        return 4
 
 
 def right_policy():
-    global ri
-    if ri % 2 == 0:
-        ri += 1
+    if np.random.uniform() > 0.5:
         return 1
     else:
-        ri += 1
-        return 2
+        return 4
+
 
 rule = {0: left_policy, 1: right_policy}
 
 
 if __name__ == "__main__":
     starts = np.array([[0, 0], [3, 0]])
-    targets = np.array([[3, 3], [0, 3]])
+    targets = np.array([[3, 0], [0, 0]])
     obstacles = np.array([])
     env = PZBoardEnv(starts, targets, obstacles, 0, lambda bot: 1)
     env.reset()
 
-    for i, agent in enumerate(env.agent_iter()):
-        print(i, agent)
+    for i, agent in enumerate(env.agent_iter(max_iter=20)):
+        print(f"Agent: {agent}; Iteration: {i}")
+        print(env.observations)
         observation, reward, done, info = env.last()
 
         action = rule[int(agent[-1])]()
