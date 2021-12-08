12cf040076f8febc3fa3fd1d563ed7e4612ff7f9
David Kraemer <david.kraemer@stonybrook.edu>
2021-12-07 20:49:19 -0500

    Implemented simple gym wrapper for pettingzoo implementation of the board environment

---
+++ src/envs/wrappers.py
@@ -0,0 +1,42 @@
+import gym
+
+
+class gym_board_env(gym.Env):
+
+    def __init__(self, env):
+        self.pz_env = env
+        self.raw_pz_env = self.pz_env.unwrapped
+
+        self.board = self.raw_pz_env.board
+
+        self.action_space = self.raw_pz_env.action_spaces
+        self.observation_space = self.raw_pz_env.observation_spaces
+
+    def reset(self):
+        self.pz_env.reset()
+        # maybe return self.state ?? unclear
+        return self.state
+
+    def step(self, actions):
+        """
+
+        Returns
+        -------
+        next_state: dict
+        reward: float
+        done: bool
+        info: dict
+        """
+
+        for action in actions:
+            self.pz_env.step(action)
+
+        done = self.board.isdone()
+        reward = self.raw_pz_env.rewards[self.raw_pz_env.agents[0]]
+
+        return self.state, reward, done, {}
+
+    @property
+    def state(self):
+        return self.raw_pz_env.observations
+
